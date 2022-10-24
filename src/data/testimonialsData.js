90b3e8eb4f495ec1dd277e26f09785f8323ad985
fardhan.mauladi <fardhan.mauladi@yokke.co.id>
2022-10-24 17:46:19 +0700

    update photo

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -1,24 +1,24 @@
 import image1 from "../assets/t-image1.png";
 import image2 from "../assets/t-image2.jpg";
-import image3 from "../assets/t-image3.jpg";
+import image3 from "../assets/fardhan-img.png";
 
 export const testimonialsData = [
   {
     image: image1,
-    review: "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: "MATHEW HENDRICKSON",
+    review: "I made the right choice by choosing the Jakarta Super Gym and by choosing the right plan and program I already achieved my ideal body!",
+    name: "EDDYTA CINTA",
     status: "ENTREPRENEUR",
   },
   {
     image: image2,
-    review: "if you want to keep your muscle strongly than before, you must go to this gymnastic ",
-    name: "NCAL",
+    review: "if you want to make your body better and healthier, then come to this gym",
+    name: "DEDDY COR.",
     status: "COACH",
   },
   {
     image: image3,
-    review: " this is so great for my body, stay healthty and hungry for muscle, Anjay",
+    review: " this is so great for my body, I really like this gym because it really helps me to increase muscle mass in my body and provide health and happiness with a reliable trainer",
     name: "FARDHAN",
-    status: "CUSTOMER",
+    status: "PROGRAMMER",
   },
 ];
